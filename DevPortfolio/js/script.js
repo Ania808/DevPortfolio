e12c3d92fafcaff36626778200b562b66e547e6c
Rozenrod <Rozenrod320@gmail.com>
2023-11-02 22:01:08 +0200

    Update script.js

--- DevPortfolio/js/script.js
+++ DevPortfolio/js/script.js
@@ -20,7 +20,7 @@ window.onscroll = () => {
 
     if(top >= offset && top < offset + height) {
       navLinks.forEach(LINKS => {
-        links.classList.remove('active');
+        LINKS.classList.remove('active');
         document.querySelector('header nav a[href*=' + id +']').classList.add('active');
       });
     };
